872dfd2017c1dba4960a3619886e874d9d6e08e4
rand <p.oscar.franzen@gmail.com>
2019-09-23 18:52:50 -0400

    introducing de.py

---
+++ adobo/de.py
@@ -0,0 +1,10 @@
+# adobo.
+#
+# Description: An analysis framework for scRNA-seq data.
+#  How to use: https://github.com/oscar-franzen/adobo/
+#     Contact: Oscar Franzen <p.oscar.franzen@gmail.com>
+"""
+Summary
+-------
+Functions for differential expression.
+"""
