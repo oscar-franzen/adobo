55da613620c2d9eaf30cd0701b314f8b951f866b
rand <p.oscar.franzen@gmail.com>
2020-01-10 20:02:52 +0100

    IO export

--- adobo/IO.py
+++ adobo/IO.py
@@ -20,9 +20,12 @@ import numpy as np
 import adobo._log
 from .data import dataset
 
-def export_data(obj, filename, norm='standard', clust='leiden', what='normalized',
-                transpose=False, sep='\t', row_names=True):
-    """Exports data to a text file, convenient for loading into other programs
+
+def export_data(obj, filename, norm='standard', clust='leiden',
+                what='normalized', transpose=False, sep='\t',
+                row_names=True):
+    """Exports data to a text file, convenient for loading into other
+    programs
 
     Parameters
     ----------
@@ -39,7 +42,8 @@ def export_data(obj, filename, norm='standard', clust='leiden', what='normalized
     transpose : `bool`
         Transpose the data before writing it. Default: False
     sep : `str`
-        A character or regular expression used to separate fields. Default: "\t"
+        A character or regular expression used to separate
+        fields. Default: "\t"
     row_names : `bool`
         Write row names or not. Default: True
 
@@ -47,8 +51,11 @@ def export_data(obj, filename, norm='standard', clust='leiden', what='normalized
     -------
     Nothing.
     """
-    if not what in ('normalized', 'clusters', 'pca', 'tsne', 'umap'):
-        raise Exception('"what" can be "normalized", "pca", "clusters", "tsne" or "umap".')
+    choices = ('normalized', 'clusters', 'pca',
+               'tsne', 'umap', 'cell_type_pred')
+    if not what in choices:
+        raise Exception(
+            '"what" must be one of: %s' % ', '.join(choices))
     if what == 'normalized':
         D = obj.norm_data[norm]['data']
     elif what == 'pca':
@@ -66,22 +73,24 @@ def export_data(obj, filename, norm='standard', clust='leiden', what='normalized
         D = D.transpose()
     D.to_csv(filename, sep=sep, index=row_names)
 
+
 def reader(filename, sep='\s', header=True, do_round=False, **args):
     """Load a gene expression matrix from a file
 
     Parameters
     ----------
     filename : `str`
-        Path to the file containing input data. Should be a matrix where
-        columns are cells and rows are genes.
+        Path to the file containing input data. Should be a matrix
+        where columns are cells and rows are genes.
     sep : `str`
-        A character or regular expression used to separate fields. Default: "\\s"
-        (i.e. any white space character)
+        A character or regular expression used to separate
+        fields. Default: "\\s" (i.e. any white space character)
     header : `bool`
         If the data file has a header or not. Default: True
     do_round : `bool`
-        In case of read count fractions, round to integers. Can be a useful remedy if
-        read counts have been imputed or similar. Default: False
+        In case of read count fractions, round to integers. Can be a
+        useful remedy if read counts have been imputed or
+        similar. Default: False
 
     Returns
     -------
@@ -91,7 +100,8 @@ def reader(filename, sep='\s', header=True, do_round=False, **args):
     skip_to_line = 1
     if header:
         skip_to_line = 2
-    count_data = dt.fread(filename, skip_to_line=skip_to_line, **args).to_pandas()
+    count_data = dt.fread(
+        filename, skip_to_line=skip_to_line, **args).to_pandas()
     count_data.index = count_data.iloc[:, 0]
     count_data = count_data.drop(count_data.columns[0], axis=1)
     if header:
@@ -105,7 +115,8 @@ def reader(filename, sep='\s', header=True, do_round=False, **args):
         elif re.search('.xz$', filename):
             tool = 'xzcat'
         cmd = '%s %s | head -n1' % (tool, filename)
-        h = subprocess.check_output(cmd, shell=True).decode('ascii').replace('\n','')
+        h = subprocess.check_output(cmd, shell=True).decode(
+            'ascii').replace('\n', '')
         if sep == '\s':
             hs = re.split('[\s,]', h)
             if len(hs) > 1:
@@ -115,7 +126,8 @@ def reader(filename, sep='\s', header=True, do_round=False, **args):
                     count_data.columns = hs[1:len(hs)]
             else:
                 if verbose:
-                    print('Skipping to set columns (mismatch in length for header).')
+                    print('Skipping to set columns (mismatch in \
+length for header).')
     # remove duplicate genes
     dups = count_data.index.duplicated(False)
     if np.any(dups):
@@ -127,47 +139,52 @@ def reader(filename, sep='\s', header=True, do_round=False, **args):
         if do_round:
             count_data = count_data.astype(int)
         else:
-            raise Exception('Non-count values detected in data matrix, consider setting \
-do_round=True, but first of all make sure your input data are raw read counts and not \
-normalized counts.')
-    rem = count_data.index.str.contains('^ArrayControl-[0-9]+', regex=True, case=False)
+            raise Exception('Non-count values detected in data matrix, consider \
+setting do_round=True, but first of all make sure your input data are raw read \
+counts and not normalized counts.')
+    rem = count_data.index.str.contains(
+        '^ArrayControl-[0-9]+', regex=True, case=False)
     count_data = count_data[np.logical_not(rem)]
     count_data.index = count_data.index.str.replace('"', '')
     count_data.columns = count_data.columns.str.replace('"', '')
     return count_data
 
-def load_from_file(filename, sep='\s', header=True, desc='no desc set', output_file=None,
-                   sparse=True, bundled=False, do_round=False, verbose=False, **args):
+
+def load_from_file(filename, sep='\s', header=True, desc='no desc set',
+                   output_file=None, sparse=True, bundled=False,
+                   do_round=False, verbose=False, **args):
     r"""Load a gene expression matrix consisting of raw read counts
-    
+
     Notes
     -----
-    The loaded gene expression matrix should not have been normalized. This function calls
-    :func:`~datatable.fread` to read the data matrix file. Any additional arguments are
-    passed into it.
+    The loaded gene expression matrix should not have been
+    normalized. This function calls :func:`~datatable.fread` to read
+    the data matrix file. Any additional arguments are passed into it.
 
     Parameters
     ----------
     filename : `str`
-        Path to the file containing input data. Should be a matrix where
-        columns are cells and rows are genes.
+        Path to the file containing input data. Should be a matrix
+        where columns are cells and rows are genes.
     sep : `str`
-        A character or regular expression used to separate fields. Default: "\\s"
-        (i.e. any white space character)
+        A character or regular expression used to separate
+        fields. Default: "\\s" (i.e. any white space character)
     header : `bool`
         If the data file has a header or not. Default: True
     desc : `str`
         A description of the data
     output_file : `str`
-        An output filename used when calling :py:func:`adobo.data.dataset.save()`.
+        An output filename used when calling
+        :py:func:`adobo.data.dataset.save()`.
     sparse : `bool`
-        Represent the data in a sparse data structure. Will save memory at the expense
-        of time. Default: True
+        Represent the data in a sparse data structure. Will save
+        memory at the expense of time. Default: True
     bundled : `bool`
         Use data installed by adobo. Default: False
     do_round : `bool`
-        In case of read count fractions, round to integers. Can be a useful remedy if
-        read counts have been imputed or similar. Default: False
+        In case of read count fractions, round to integers. Can be a
+        useful remedy if read counts have been imputed or
+        similar. Default: False
     verbose : `bool`
         To be verbose or not. Default: False
 
@@ -178,15 +195,17 @@ def load_from_file(filename, sep='\s', header=True, desc='no desc set', output_f
     """
     if bundled:
         if re.search('/', filename):
-            raise Exception('If bundled=True, just specify a file name, not a path.')
-        filename = '/'.join(adobo._log.__file__.split('/')[0:-1]) + '/data/' + filename
+            raise Exception(
+                'If bundled=True, just specify a file name, not a path.')
+        filename = '/'.join(adobo._log.__file__.split('/')
+                            [0:-1]) + '/data/' + filename
     if not os.path.exists(filename):
         raise Exception('%s not found' % filename)
     stime = time.time()
     #count_data = pd.read_csv(filename, delimiter=sep, header=header, **args)
     count_data = reader(filename, sep, header, do_round, **args)
-    obj = dataset(count_data, desc, output_file=output_file, input_file=filename,
-                  sparse=sparse, verbose=verbose)
+    obj = dataset(count_data, desc, output_file=output_file,
+                  input_file=filename, sparse=sparse, verbose=verbose)
     if verbose:
         genes = '{:,}'.format(count_data.shape[0])
         cells = '{:,}'.format(count_data.shape[1])
